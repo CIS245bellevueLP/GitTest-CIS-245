d52778a705820e91e4f9fb70a8fec42f64feebb0
CIS245bellevueLP <79030404+CIS245bellevueLP@users.noreply.github.com>
2021-02-13 19:55:57 -0600

    Add files via upload

---
+++ hello_git.py
@@ -0,0 +1,6 @@
+Python 3.8.6 (default, Sep 25 2020, 09:36:53) 
+[GCC 10.2.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('hello Git World')
+hello Git World
+>>> 
